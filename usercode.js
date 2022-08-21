fe2068f0ec9f9a57fcf720f2a988dff949c344cb
Yevhen Kysilov <yevhen-k@MacBook-Pro-Yevhen-2.local>
2022-08-21 15:38:03 +0100

    add crypto-js

--- usercode.js
+++ usercode.js
@@ -1,7 +1,8 @@
 const SHA256 = require("crypto-js/sha256");
 
 module.exports = (data) => {
-    data.hash = SHA256(data.payload);
-
+    data.hash = SHA256(data.payload).toString();
+    delete data.payload;
+    
     return data;
 }
